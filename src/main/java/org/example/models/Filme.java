39a04576871167a19d3dc70eab199908ec9c9903
Iarley Souza <iarleysouza615@gmail.com>
2024-12-31 12:34:58 -0300

    add ex

--- src/main/java/org/example/models/Filme.java
+++ src/main/java/org/example/models/Filme.java
@@ -1,12 +1,19 @@
 package org.example.models;
 
-public class Filme extends Titulo{
+import org.example.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
     public String getDiretor() {return diretor;}
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return 0;
     }
 }
 
